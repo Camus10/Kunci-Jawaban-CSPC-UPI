448a1edd2d1c17c3502e46d214459e707fd774d3
Camus <70552281+Camus10@users.noreply.github.com>
2021-01-03 03:50:26 +0700

    Add files via upload

---
+++ gorenganPakEngkus.js
@@ -0,0 +1,9 @@
+let inputGehu = 2;
+let inputBalaBala = 2;
+let inputPisangGoreng = 2;
+let inputCireng = 2;
+let inputHariMenjual = 2;
+
+console.log(
+    ((inputGehu * 100) + (inputBalaBala * 150) + (inputPisangGoreng * 75) + (inputCireng * 175)) * inputHariMenjual
+);
