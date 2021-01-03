f260cd9211e27e69149ecbdfd34541d7b1a578d5
Camus <70552281+Camus10@users.noreply.github.com>
2021-01-03 16:55:32 +0700

    Update deskripsiBinerSuperCepot.js

--- Algoritma Pemograman 1/deskripsiBinerSuperCepot.js
+++ Algoritma Pemograman 1/deskripsiBinerSuperCepot.js
@@ -1,11 +1,11 @@
-let code = "000000010000101100010101"
+let code = "000000010000101100010101";
 let spoken = [];
 let counter, getStart = 0, getEnd = 8;
 let wordlist = [
     null, "a", "b", "c", "d", "e", "f", "g", "h",
     "i", "j", "k", "l", "m", "n", "o", "p", "q",
     "r", "s", "t", "u", "v", "w", "x", "y", "z"
-]
+];
 let wordFound = "";
 
 for(counter = 0; counter < code.length / 8; counter++){
